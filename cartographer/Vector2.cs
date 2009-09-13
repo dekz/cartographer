00f48962761c3d8db8a54e598ad7916c7bdc82ff
eternal <eternal@feistystudios.com>
2009-09-13 14:17:31 +1000

    fuck you properties

---
+++ cartographer/Vector2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cartographer
+{
+    public class Vector2
+    {
+        public int X;
+        public int Y;
+    }
+}
